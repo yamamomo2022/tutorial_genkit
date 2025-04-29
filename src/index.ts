c1ad2a8d2ac36d09c6e877e05f119e4ffcfa2018
viole <violetraisondetre@gmai.com>
2025-04-29 21:40:25 +0900

    feat: add genkit-cli dependency and implement helloFlow
    - Added "genkit-cli" as a dependency in package.json.
    - Created a new prompt file "hello.prompt" for the helloFlow.
    - Implemented helloFlow in index.ts to greet users and return a response from the AI.

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,32 @@ export const googleAIapiKey = process.env.GOOGLE_GENAI_API_KEY || defineSecret(`
 
 // Initialize Genkit
 export const ai = genkit({
-  plugins: [googleAI(),  mapsClient ],
-  model: gemini25FlashPreview0417,  
+  plugins: [googleAI(), mapsClient],
+  model: gemini25FlashPreview0417,
 })
 
 
 const { googleMapsFlow } = require('./google-maps-flow');
 
 export const callGoogleMaps = onCallGenkit({
-  secrets: [ googleAIapiKey ],
+  secrets: [googleAIapiKey],
   region: 'asia-northeast1',
   cors: true,
 },
   googleMapsFlow
 )
+
+
+export const helloFlow = ai.defineFlow(
+  {
+    name: "hello",
+  },
+  async () => {
+
+    const helloPrompt = ai.prompt('hello');
+    const response = await helloPrompt();
+
+    console.log("Chat Response:", response);
+    return response.text;
+  }
+);
